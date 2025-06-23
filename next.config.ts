c034734ce80d8cd4ce62a05b029ae34659e4adfe
mshtwtnb0219 <b77yellow@gmail.com>
2025-06-23 21:21:28 +0900

    prisma setting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 🔑 ここが超重要！
+  transpilePackages: ['@prisma/client'],
+
+  // 外部画像扱う場合は追記が必要
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
